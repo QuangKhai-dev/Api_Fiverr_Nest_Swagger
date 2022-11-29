ba4dd17302568b4bf417c56cfeea5430c5169069
khai do <quangkhai0901@gmail.com>
2022-11-29 18:23:04 +0700

    check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [AuthModule, PrismaModule, UserModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
